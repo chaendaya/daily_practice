a97fa9d07739c61030e1583547aec7f3bd024bfc
chaendaya <114456343+chaendaya@users.noreply.github.com>
2022-10-18 17:37:44 +0900

    Update Quadrant two.cpp

--- Quadrant two.cpp
+++ Quadrant two.cpp
@@ -2,25 +2,25 @@
 #include <cmath>
 using namespace std;
 
-int dist_2d(int x1, int y1, int x2, int y2) {
+double dist_2d(double x1, double y1, double x2, double y2) {
 
-	int sum, dist;
-	sum = pow(x2 - x1, 2) + pow(y2 - y1, 2);
-	dist = sqrt(sum);
+	double dist;
+
+	dist = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 
 	return dist;
-	
 }
+
 int main() {
 
-	int x1, x2, y1, y2;
+	double x1, x2, y1, y2;
 	cout << "input x1, y1 : ";
 	cin >> x1 >> y1;
 
 	cout << "input x2, y2 : ";
 	cin >> x2 >> y2;
 
-	cout << "Distance : " << dist_2d(x1, y1, x2, y2);
+	cout << "Distance : " << dist_2d(x1, y1, x2, y2) << endl;
 
 	return 0;
 }
